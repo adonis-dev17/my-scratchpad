ccf74d94fc6beb1c527c96e6f6522318a9cae843
Adonis Medrocillo <adonis.dev17@gmail.com>
2024-05-11 17:35:26 +0800

    Initial commit.

---
+++ js/main.js
@@ -0,0 +1,36 @@
+// const greetings = { word1: 'hello', word2: 'world!'}
+
+// let greet = () => {return greetings['word1'] + ' ' + greetings['word2'];}
+
+// document.getElementById('demo').innerHTML = greet();
+
+// const greetings = { word1: 'Hello', word2: 'World', greet: function() { return this.word1 + ' ' + this.word2;}}
+// document.getElementById('demo').innerHTML = greetings.greet()
+
+// const shout = { rap1: 'HipHop', rap2: 'Hooray!', combine: function() { return this.rap1 + ' ' + this.rap2;}}
+// document.getElementById('demo').innerHTML = shout.combine();
+
+// document.getElementById('showtime').addEventListener('click', clicker)
+
+// function clicker() {
+//   this.innerHTML = Date();
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
