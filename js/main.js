1ebfb38026e764048f6a56b71b9f3a927ff90039
Adonis Medrocillo <adonis.dev17@gmail.com>
2024-05-27 12:41:34 +0800

    Add Nested Objects,7.1 create a varaible capFave and assign favorite foods.

--- js/main.js
+++ js/main.js
@@ -76,5 +76,35 @@ takeOff() {
 alienShip.retreat();
 alienShip.takeOff();
 
-
+//7. Nested Objects
+let spaceship = {
+  passengers: null,
+  telescope: {
+    yearBuilt: 2018,
+    model: "91031-XLT",
+    focalLength: 2032 
+  },
+  crew: {
+    captain: { 
+      name: 'Sandra', 
+      degree: 'Computer Engineering', 
+      encourageTeam() { console.log('We got this!') },
+     'favorite foods': ['cookies', 'cakes', 'candy', 'spinach'] }
+  },
+  engine: {
+    model: "Nimbus2000"
+  },
+  nanoelectronics: {
+    computer: {
+      terabytes: 100,
+      monitors: "HD"
+    },
+    'back-up': {
+      battery: "Lithium",
+      terabytes: 50
+    }
+  }
+}; 
+//7.1 Create a variable capFave and assign the captain‘s favorite food (the element in the 0th index of her 'favorite foods' array) to it. Make sure to use bracket and dot notation to get the value of the food through nested access (don’t just copy the value into the variable!)
+let capFave = spaceship.crew.captain['favorite foods'][0];
 
